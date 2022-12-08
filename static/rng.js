9df72d2ab0cc93d4fc3a236029dfaaec5de93e3f
c1027525Newcastle <c1027525@newcastle.ac.uk>
2022-12-08 12:30:56 +0000

    Updated some comments

--- static/rng.js
+++ static/rng.js
@@ -12,13 +12,12 @@ function luckyDip() {
     for (let i = 0; i < randomBuffer.length; i++){
         randomBufferFloat[i] = randomBuffer[i] / (0xFFFFFFFF);
     }
-
-    let count = 0;
     //
 
     let min;
     let max;
     let value;
+    let count = 0;
     // while set does not contain 6 values, create a random value between 1 and 60
     while (draw.size < 6) {
         // COMMENT
@@ -26,15 +25,13 @@ function luckyDip() {
         min = Math.ceil(1);
         max = Math.floor(60);
         value = Math.floor(csRandomNumber * (max - min + 1) + min);
-        // COMMENT Maybe create a function from this just to make it more efficient?
+        // Check is value is already in draw and make a new cryptographic secure one
         while (draw.has(value)) {
             let newRandomBuffer = new Uint32Array(1);
             window.crypto.getRandomValues(newRandomBuffer);
             let newcsRandomNumber = newRandomBuffer[0] / (0xFFFFFFFF);
             value = Math.floor(newcsRandomNumber * (max - min + 1) + min);
-            console.log("newValue=", value)
         }
-        console.log("Passed the while loop with array")
         count += 1;
         // sets cannot contain duplicates so value is only added if it does not exist in set
         draw.add(value);
